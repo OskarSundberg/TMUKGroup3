69d083d4f843fc88b3626ec5ddaad6e41c3a6a34
ivonilsson <ivo_gote@hotmail.com>
2023-01-24 19:50:31 +0100

    Added User class and it's components

---
+++ Server/Server/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    internal class User
+    {
+        private string name;
+        private string ipAddress;
+        private bool isOnline;
+
+        public bool IsOnline
+        {
+            get { return isOnline; }
+            set { isOnline = value; }
+        }
+
+        public User(string ipAddress, string name)
+        {
+            this.ipAddress = ipAddress;
+            this.name = name;
+        }
+    }
+}
