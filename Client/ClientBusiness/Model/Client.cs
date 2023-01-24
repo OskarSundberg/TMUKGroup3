2df408510931fe1bdc93a4b1c34332e26b41d3a8
Oskar Sundberg <oskarsundberg@live.se>
2023-01-24 18:05:52 +0100

    Added INotifyPropertyChanged interface to Client

--- Client/ClientBusiness/Model/Client.cs
+++ Client/ClientBusiness/Model/Client.cs
@@ -1,12 +1,27 @@
 ﻿namespace ClientBusiness.Model
 {
     using System;
+    using System.ComponentModel;
     using System.Net;
     using System.Net.Sockets;
+    using System.Runtime.CompilerServices;
     using System.Text;
 
-    public class Client
+    public class Client : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value == name)
+                    return;
+                name = value;
+                OnPropertyChanged();
+            }
+        }
         public void StartClient()
         {
             byte[] bytes = new byte[1024];
@@ -55,6 +70,11 @@
             {
                 Console.WriteLine(e.ToString());
             }
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
