a611312dd5b6c040f3318c5c41dde07c73255e22
Simon de Reuver <dereuversimon@gmail.com>
2023-01-26 12:28:18 +0100

    Added NUnit testing to Client project

---
+++ Client/ClientNUnitTest/ClientTests.cs
@@ -0,0 +1,28 @@
+namespace ClientNUnitTest
+{
+    public class Test
+    {
+        [SetUp]
+        public void Setup()
+        {
+            //I setup kan vi istället instansiera vårat MathClass math variabel
+        }
+
+
+        //Demonstraion av ett test man kan köra
+        [Test]
+        public void TestExample()
+        {
+            // ARRANGE
+            //var MathClass math = new MathClass(); 
+
+            // ACT
+            //int result = math.Add(1,1);
+
+            // ASSERT
+            //här använder vi assert för att se att resultatet är det vi förväntar oss att det ska vara, dvs att Add av 1+1 = 2
+            // Assert.AreEqual(2, result);
+            Assert.Fail();
+        }
+    }
+}
---
+++ Client/ClientUnitTesting/UnitTest1.cs
@@ -0,0 +1,12 @@
+using ClientBusiness.Model;
+namespace ClientUnitTesting
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
