a3e84293b34f41e04bbdc009e38f7ac866513dde
Samuel Wallander Leyonberg <samuel.leyonberg@gmail.com>
2023-01-26 14:55:52 +0100

    Added NUnit testing for user and Echo method

--- Server/ServerNUnit/UnitTest1.cs
+++ Server/ServerNUnit/UnitTest1.cs
@@ -1,17 +1,35 @@
 using NUnit.Framework;
+using System.Net.WebSockets;
+using Server;
+using System.Net.Sockets;
+
 namespace ServerNUnit
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void Echo_Testing()
         {
+            //ARANGE
+            var echoTest = new Allchat();
+
+            //ACT
+            echoTest.Echo("");
+
+            //ASSERT
+            Assert.AreEqual(1, echoTest.Echo(""));
         }
 
         [Test]
-        public void Test1()
+        public void User_Test()
         {
-            Assert.Pass();
+            var user = new User("Sam", null);
+
+            user.Name = "Test";
+            user.IsOnline = false;
+
+            Assert.AreEqual("Test", user.Name);
+            Assert.AreEqual(false, user.IsOnline);
         }
     }
 }
