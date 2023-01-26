527701839544ff4078777530569d5bce59548d13
Gustav Pråmell <gustavpra@gmail.com>
2023-01-26 23:47:50 +0100

    Make testserver in test setup to simulate a real connection

--- Server/ServerNUnit/UnitTest1.cs
+++ Server/ServerNUnit/UnitTest1.cs
@@ -2,36 +2,66 @@ using NUnit.Framework;
 using System.Net.WebSockets;
 using Server;
 using System.Net.Sockets;
+using System.Net;
+using System.Reflection;
 
 namespace ServerNUnit
 {
     public class Tests
     {
-        Allchat echoTest;
+        Allchat allChatTest;
         User testUserOne;
         User testUserTwo;
+        Socket senderOne;
+        Socket senderTwo;
+        Socket listener;
 
         [SetUp]
         public void Setup()
         {
-            echoTest = new Allchat();
-            testUserOne = new User("Sam", null);
-            testUserTwo = new User("Samme", null);
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            IPEndPoint server = new IPEndPoint(ipAddress, 13375);
+
+            listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            listener.Bind(server);
+            listener.Listen(10);
+
+            senderOne = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            senderOne.Connect(server);
+
+            senderTwo = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            senderTwo.Connect(server);
+
+            allChatTest = new Allchat();
+            testUserOne = new User("Sam", senderOne);
+            testUserTwo = new User("Samme", senderTwo);
         }
 
         [Test]
         public void Echo_Testing()
         {
             //ASSERT
-            Assert.AreEqual(1, echoTest.Echo("123"));
+            Assert.AreEqual(1, allChatTest.Echo("123"));
         }
 
         [Test]
         public void User_Test()
         {
-            Assert.IsNull(testUserTwo.Handler);
+            Assert.IsNotNull(testUserOne.Handler);
             Assert.AreEqual("Sam", testUserOne.Name);
             Assert.AreEqual(false, testUserOne.IsOnline);
         }
+        [Test]
+        public void UserJoin_Test()
+        {
+            try
+            {
+                allChatTest.UserJoin(testUserOne);
+            }
+            catch(Exception ex) 
+            { 
+                Assert.Fail(ex.Message);
+            }
+        }    
     }
 }
