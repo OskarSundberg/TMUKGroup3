1ea63a68bc5fff974e7de2add8da01c5ae18d391
Gustav Pråmell <gustavpra@gmail.com>
2023-01-27 22:37:08 +0100

    Fixed random error code

--- Client/ClientNUnitTest/ClientTests.cs
+++ Client/ClientNUnitTest/ClientTests.cs
@@ -8,23 +8,6 @@ namespace ClientNUnitTest
             //I setup kan vi istället instansiera vårat MathClass math variabel
         }
 
-
-        //Demonstraion av ett test man kan köra
-        [Test]
-        public void TestExample()
-        {
-            // ARRANGE
-            //var MathClass math = new MathClass(); 
-
-            // ACT
-            //int result = math.Add(1,1);
-
-            // ASSERT
-            //här använder vi assert för att se att resultatet är det vi förväntar oss att det ska vara, dvs att Add av 1+1 = 2
-            // Assert.AreEqual(2, result);
-            Assert.Fail();
-        }
-
         [Test] 
         public void TestClientName() 
         {
