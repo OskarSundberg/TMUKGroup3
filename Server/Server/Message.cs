335d01a8b24d8311cf605cdd71fe5e3c46998bb5
Gustav Pråmell <gustavpra@gmail.com>
2023-02-21 20:14:59 +0100

    Added Serialize and Deserialize methods

--- Server/Server/Message.cs
+++ Server/Server/Message.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,6 +19,25 @@ namespace Server
         {
             this.msg = msg;
             this.userFrom = userFrom;
+        }
+        public byte[] SerializeMsg(Message msg)
+        {
+            BinaryFormatter binaryFormatter= new BinaryFormatter();
+            using (MemoryStream stream = new MemoryStream())
+            {
+                binaryFormatter.Serialize(stream, msg);
+                return stream.ToArray();
+            }
+        }
+        public Object DeserializeMsg(byte[] bytes)
+        {
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            using (MemoryStream stream = new MemoryStream())
+            {
+                stream.Write(bytes);
+                Object obj = binaryFormatter.Deserialize(stream);
+                return obj;
+            }
         }
     }
 }
