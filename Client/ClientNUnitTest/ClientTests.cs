c5eb3df76148c8287536ac3cbfe6bc9b91ec33c8
Oskar Sundberg <oskarsundberg@live.se>
2023-02-22 14:22:03 +0100

    Fixing and cleaning

--- Client/ClientNUnitTest/ClientTests.cs
+++ Client/ClientNUnitTest/ClientTests.cs
@@ -73,37 +73,12 @@ namespace ClientNUnitTest
         [Test]
         public void Send_message_Test()
         {
-            //Thread t2 = new Thread(() =>
-            //{
-            //    MainWindow = new MainWindow("test");
-            //});
-            //t2.SetApartmentState(ApartmentState.STA);
-            //t2.Start();
-            //t2.Join();
             Assert.IsTrue(MainWindow.Send_message("test"));
         }
 
         [Test]
         public void SpamFilter_Test()
         {
-            //Thread t = new Thread(() =>
-            //{
-            //    MainWindow = new MainWindow("test");
-
-            //    //System.Windows.Threading.Dispatcher.Run();
-
-            //    MainWindow.Time = DateTime.Now;
-
-
-            //    Thread.Sleep(1000);
-            //    Assert.IsTrue(MainWindow.SpamFilter());
-
-            //    MainWindow.Time = DateTime.Now;
-            //    Assert.IsFalse(MainWindow.SpamFilter());
-            //});
-            //t.SetApartmentState(ApartmentState.STA);
-            //t.Start();
-            //t.Join();
             MainWindow.Time = DateTime.Now;
             Thread.Sleep(1000);
             Assert.IsTrue(MainWindow.SpamFilter());
