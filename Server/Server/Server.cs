04b133a2b0dc0f6c44ec11e1914f7391285c5e0b
Gustav Pråmell <gustavpra@gmail.com>
2023-02-24 02:58:53 +0100

    Some code cleanup work

--- Server/Server/Server.cs
+++ Server/Server/Server.cs
@@ -11,7 +11,7 @@ namespace Server
     {
         public static IPAddress? GetIPAddress { get; set; }
         public static Allchat allchat = new Allchat();
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             StartServer();
             return;
@@ -23,45 +23,45 @@ namespace Server
         /// <param></param>
         public static void StartServer()
         {
-            string? hostName = Dns.GetHostName();
-            IPHostEntry? iPHostEntry = Dns.GetHostEntry(hostName);
-            IPAddress? iPAddress = iPHostEntry.AddressList.FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork);
-            GetIPAddress = iPAddress;
-            int port = 13375;
-            IPEndPoint? iPEndPoint = new IPEndPoint(iPAddress, port);
-            int dataPort = 31337;
-            IPEndPoint iPEndPointData = new IPEndPoint(iPAddress, dataPort);
-            Console.WriteLine("Please connect to IP: {0} and port: {1}", iPAddress, port);
-
+            IPHostEntry? iPHostEntry = Dns.GetHostEntry(Dns.GetHostName());
+            GetIPAddress = iPHostEntry.AddressList.FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork);
+            IPEndPoint? iPEndPoint = new IPEndPoint(GetIPAddress, 13375);
+            IPEndPoint iPEndPointData = new IPEndPoint(GetIPAddress, 31337);
+            Console.WriteLine("Please connect to IP: {0} and port: {1}", GetIPAddress, 13375);
             try
             {
-                Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-                Socket listenerData = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-                listener.Bind(iPEndPoint);
-                listener.Listen(100);
-                listenerData.Bind(iPEndPointData);
-                listenerData.Listen(100);
-
+                Socket listener = StartSocket(iPEndPoint);
+                Socket listenerData = StartSocket(iPEndPointData);
                 while (true)
                 {
-                    Console.WriteLine("Waiting for anything");
-                    Socket handler = listener.Accept();
-                    byte[] bytes = new byte[64000];
-                    int bytesRead = handler.Receive(bytes);
-                    string name = Encoding.UTF8.GetString(bytes, 0, bytesRead);
-                    User user = new User(name, handler);
-                    Console.WriteLine("Waiting for connection to data port");
-                    Socket dataHandler = listenerData.Accept();
-                    user.DataHandler = dataHandler;
-                    allchat.UserJoin(user);
-                    Console.WriteLine("Sent connection to session");
-
+                    CreateUser(listener, listenerData);
                 }
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.ToString());
             }
+        }
+        private static void CreateUser(Socket listener, Socket listenerData)
+        {
+            Console.WriteLine("Waiting for anything");
+            Socket handler = listener.Accept();
+            byte[] bytes = new byte[64000];
+            int bytesRead = handler.Receive(bytes);
+            string name = Encoding.UTF8.GetString(bytes, 0, bytesRead);
+            User user = new User(name, handler);
+            Console.WriteLine("Waiting for connection to data port");
+            Socket dataHandler = listenerData.Accept();
+            user.DataHandler = dataHandler;
+            allchat.UserJoin(user);
+            Console.WriteLine("Sent connection to session");
+        }
+        private static Socket StartSocket(IPEndPoint? iPEndPoint)
+        {
+            Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            listener.Bind(iPEndPoint);
+            listener.Listen(100);
+            return listener;
         }
     }
 }
